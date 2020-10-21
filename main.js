b016354678c85ccbdfc780a22ba0aa41df0499f4
Wojtek Kaźmierczak <44867612+wordydeer@users.noreply.github.com>
2020-10-21 09:36:46 +0200

    stage4

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const btn = document.querySelector(".btn");
+const btn2 = document.querySelector(".btn2");
 const d = document.querySelector(".d");
 
 btn.addEventListener("click", function(){
     d.className = "d2";
-})
+});
