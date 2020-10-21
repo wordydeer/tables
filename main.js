8728c44b58300c85e96b5c5a44502c7118f8ec8c
Wojtek Kaźmierczak <44867612+wordydeer@users.noreply.github.com>
2020-10-21 12:09:20 +0200

    stage8

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
-const btn = document.querySelector(".btn");
-const btn2 = document.querySelector(".btn2");
-const d = document.querySelector(".d");
+var modal = document.getElementById("myModal");
 
-btn.addEventListener("click", function(){
-    d.className = "d2";
-});
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+  console.log("test");
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
